e8d5bfe3eff3ad3599e954c253c5a084d05e23f5
Hamster-ux <p86913237@gmail.com>
2026-06-18 12:05:33 +0900

    Retire legacy service worker and clear old caches

--- civil-timetable/sw.js
+++ civil-timetable/sw.js
@@ -1,1 +1,1 @@
-importScripts('./sw-v24.js?v=24');
+self.addEventListener('install',event=>{self.skipWaiting()});self.addEventListener('activate',event=>{event.waitUntil(Promise.all([self.registration.unregister(),caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key))))]).then(()=>self.clients.claim()))});
